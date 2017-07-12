0f9fe91b03767ee3d5335cfcfa91d63164d2f830
Colin Goutte <cgte@bk.ru>
2017-07-12 13:58:03 +0200

    Look for remote and local branches

--- git_not_merged_branches.sh
+++ git_not_merged_branches.sh
@@ -1,13 +1,14 @@
 
 #cleans output from git
 clean() {
-             cut -c 3- | sed 's:^origin/::' | grep -v  '^master$' | grep -v '^HEAD';}
+             cut -c 3- |  sed 's:^remotes/::' | sed 's:^origin/::' | grep -v  '^master$' | grep -v '^HEAD';
+        }
 
 # Gets you all the branch that contain the latest tag
-f() { git tag | sort -rV | sed '5q;d' | xargs git branch -r --contains ; }
+f() { git tag | sort -rV | sed '5q;d' | xargs git branch -a --contains ; }
 
 #gets unmerged branch
-g() { git branch -r --no-merged 'master' ; }
+g() { git branch -a --no-merged 'master' ; }
 
 # Takes common parts since it g can ouput very old divreged branches.
 # and f may give you branches that are already berged in master
